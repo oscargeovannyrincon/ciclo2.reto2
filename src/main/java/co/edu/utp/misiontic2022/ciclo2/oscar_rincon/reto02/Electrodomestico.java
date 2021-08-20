2c34961cd4845951950184163a0f069646c636a3
oscargeovannyrincon <86750512+oscargeovannyrincon@users.noreply.github.com>
2021-08-20 01:56:14 -0500

    Creacion de contructores

--- src/main/java/co/edu/utp/misiontic2022/ciclo2/oscar_rincon/reto02/Electrodomestico.java
+++ src/main/java/co/edu/utp/misiontic2022/ciclo2/oscar_rincon/reto02/Electrodomestico.java
@@ -2,12 +2,49 @@ package co.edu.utp.misiontic2022.ciclo2.oscar_rincon.reto02;
 
 public class Electrodomestico {
 
+    //CONSTANTES
     private final Double PRECIO_BASE=100.0;
     private final Character CONSUMO_W_BASE='F';
     private final Integer PESO_BASE=5;
+
+    //ATRIBUTOS
+    private Double precioBase;
     private Integer peso;
     private Character consumoW;
-    private Double precioBase;
+
+
+    //CONSTRUCTORES
+    public Electrodomestico() {
+        this.peso=PESO_BASE;
+        this.consumoW=CONSUMO_W_BASE;
+        this.precioBase=PRECIO_BASE;
+    }
+
+    public Electrodomestico(Double precioBase, Integer peso) {
+        this.precioBase = precioBase;
+        comprobarPeso(peso);
+    }
+
+    
+
+    
+
+    public Electrodomestico(Double precioBase, Integer peso, Character consumoW) {
+        this.precioBase = precioBase;
+        comprobarPeso(peso);
+        comprobarConsumoW(consumoW);
+    }
+
+    private void comprobarConsumoW(Character consumoW) {
+    }
+
+    private void comprobarPeso(Integer peso) {
+
+    }
+
+    
+
+    
     
     
 }
