51e013216cb684ecd8de815a6299790edcac8269
oscargeovannyrincon <86750512+oscargeovannyrincon@users.noreply.github.com>
2021-08-19 20:10:19 -0500

    creacion clase electrodomestico

---
+++ src/main/java/co/edu/utp/misiontic2022/ciclo2/oscar_rincon/reto02/Electrodomestico.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic2022.ciclo2.oscar_rincon.reto02;
+
+public class Electrodomestico {
+
+    private final Double PRECIO_BASE=100.0;
+    private final Character CONSUMO_W_BASE='F';
+    private final Integer PESO_BASE=5;
+    private Integer peso;
+    private Character consumoW;
+    private Double precioBase;
+    
+    
+}
