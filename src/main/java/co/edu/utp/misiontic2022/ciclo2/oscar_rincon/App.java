97c1c71a10f8db134e2af7b28cc21bab73048963
oscargeovannyrincon <86750512+oscargeovannyrincon@users.noreply.github.com>
2021-08-19 20:07:13 -0500

    Commit inicial

---
+++ src/main/java/co/edu/utp/misiontic2022/ciclo2/oscar_rincon/App.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic2022.ciclo2.oscar_rincon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
